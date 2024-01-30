a5b0bac369617c145bc3c88fda2beba60761e267
Saks <sakshemgotekar@gmail.com>
2024-01-30 14:18:13 +0530

    Changes in app

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ st.title('Anime Recommendation Application using 3 Approaches :japanese_goblin:'
 st.subheader('Based on the Anime :movie_camera: & Recommendation Engine :rocket: You Select, We will Recommend :smiling_face_with_3_hearts: you another 5 Animes :star:  to Binge Watch! '':heart_eyes:')
 st.image("Animewallpaper.png", width=1050)
 st.header('Please choose Recommendation :rocket: Engine: ')
-st.warning('Please Wait Patiently while the Background Process is Complete :turtle:')
 
 
 engine = st.radio('Select Any of the Recommendation Models below:',
