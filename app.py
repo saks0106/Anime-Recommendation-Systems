d7181d7609d7a477221aa78e332ce13232ddf497
Saks <sakshemgotekar@gmail.com>
2024-01-30 23:34:28 +0530

    Changed custom_type file

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 import streamlit as st
 import pandas as pd
 import pickle
--- custom_typed.py
+++ custom_typed.py
@@ -3,6 +3,7 @@ from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import warnings
+import random
 
 import string
 import tensorflow as tf
@@ -15,7 +16,7 @@ df_anime = pd.read_csv('df_anime.csv')
 
 
 class CustomUser:
-
+    SimilarityArr = []
     def standardization(self,data):
         x = tf.strings.lower(data)  # all strings to lower
         x = tf.strings.regex_replace(x, "<[^>]+>", "")  # removing html
@@ -26,32 +27,51 @@ class CustomUser:
         return x
 
     def requirementbased(self,user_input_genres, scores, features):
+        self.df_anime2 = df_anime.copy()
         try:
             features = self.standardization(features)
             eng_sw = stopwords.words('english')
             features_token = word_tokenize(features)
             features_token_set = set([ps.stem(token) for token in features_token if token not in eng_sw])
 
+            if len(user_input_genres) != 0:
+                genre_pattern = '|'.join(user_input_genres)
+                genres_selected = self.df_anime2['Genres'].str.contains(genre_pattern)
+                self.df_anime2 = self.df_anime2[genres_selected]
 
-            genre_pattern = '|'.join(user_input_genres)
-            genres_selected = df_anime['Genres'].str.contains(genre_pattern)
-            df_temp = df_anime[genres_selected]
-            reqbased = df_temp[df_temp['Score'] >= scores].sort_values(by='Score', ascending=False)
-            reqbased = reqbased.reset_index(drop=True)
+            self.df_anime2 = self.df_anime2[self.df_anime2['Score'] >= scores].sort_values(by='Score', ascending=False)
+            self.df_anime2 = self.df_anime2.reset_index(drop=True)
 
-            common_count = []
-            for i in range(reqbased.shape[0]):
-                summary = self.standardization(reqbased['Synopsis'][i])
+
+            for i in range(self.df_anime2.shape[0]):
+                summary = self.standardization(self.df_anime2['Synopsis'][i])
                 summary_features_token = word_tokenize(summary)
                 combo_summary_token_set = set([ps.stem(token) for token in summary_features_token if token not in eng_sw])
-                similarity = len(features_token_set.intersection(combo_summary_token_set))
-                reqbased['Similarity'] = "{:.2f}%".format(similarity * 100)
-                common_count.append(len(features_token_set.intersection(combo_summary_token_set)))
+                self.df_anime2['Similarity'] = len(features_token_set.intersection(combo_summary_token_set))
+            self.df_anime2 = self.df_anime2.sort_values(by='Similarity', ascending=False)
 
-            reqbased['Similarity_Count'] = common_count
-            reqbased = reqbased.sort_values(by='Similarity_Count', ascending=False)#.head(31)
-            reqbased = reqbased[['Name','Similarity','Genres','Synopsis','Other name','Score','Episodes','Favorites','Image URL']]
-            return list(reqbased.to_dict('records'))
+            for i in range(30):
+                anime_name = self.df_anime2['Name'].values[i]
+                similarity = self.df_anime2['Similarity'].values[i]
+                genre = self.df_anime2['Genres'].values[i]
+                synopsis = self.df_anime2['Synopsis'].values[i]
+                local_name = self.df_anime2['Other name'].values[i]
+                score = self.df_anime2['Score'].values[i]
+                episodes = self.df_anime2['Episodes'].values[i]
+                fav = self.df_anime2['Favorites'].values[i]
+                url = self.df_anime2['Image URL'].values[i]
+
+                CustomUser.SimilarityArr.append({"Name": anime_name,
+                                      "Similarity": similarity,
+                                      "Genres": genre,
+                                      "Synopsis": synopsis,
+                                      "Other name": local_name,
+                                      'Score': score,
+                                      'Episodes': episodes,
+                                      'Favorites': fav,
+                                      'Image URL': url})
+
+            return CustomUser.SimilarityArr
 
         except:
             print(f"{features} not found!. Please Try Again")
